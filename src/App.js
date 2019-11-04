07cb2fc2bed4bd8936bbfcf8e7c12ff6f9591371
Gerd Müller <g.mueller@ewerk.com>
2019-11-04 14:29:34 +0100

    Add mobile first approach

--- src/App.js
+++ src/App.js
@@ -15,18 +15,18 @@ function App() {
             </div>
           </div>
           <div className="parallax__layer parallax__layer--fore">
-            <div className="text__action">Scrolle nach unten! &dArr;</div>
+            <div className="text__action"><h2>Scrolle nach unten! &dArr;</h2></div>
           </div>
         </div>
         <div id="chapter2" className="parallax__group">
           <div className="parallax__layer parallax__layer--base">
-            <div className="title box">Es ist 1927. Irgendwo im Ruhrgebiet leben zwei Kinder: Anna und Karl. Die Eltern der beiden gehören zur großen Arbeiterschaft, die im Bergbau, in Webereien, Fabriken und Stahlwerken arbeiten. Anna und Karl machen sich Gedanken über ihre Zukunft, denn sie sind 13 Jahre alt und ihr letztes Schuljahr steht bald bevor. Begleite sie dabei, wie sie ihren Weg zu einer „Kinderrepublik“ der sozialistischen Arbeiterjugend finden und was sie dort erleben.</div>
+            <div className="title box"><p>Es ist 1927. Irgendwo im Ruhrgebiet leben zwei Kinder: Anna und Karl. Die Eltern der beiden gehören zur großen Arbeiterschaft, die im Bergbau, in Webereien, Fabriken und Stahlwerken arbeiten. Anna und Karl machen sich Gedanken über ihre Zukunft, denn sie sind 13 Jahre alt und ihr letztes Schuljahr steht bald bevor. Begleite sie dabei, wie sie ihren Weg zu einer „Kinderrepublik“ der sozialistischen Arbeiterjugend finden und was sie dort erleben.</p></div>
           </div>
           <div className="parallax__layer parallax__layer--back"></div>
         </div>
         <div id="chapter3" className="parallax__group">
           <div className="parallax__layer parallax__layer--fore">
-          <div className="title box">Wen möchtest du auf diesem Weg begleiten?</div>
+          <div className="title box"><p>Wen möchtest du auf diesem Weg begleiten?</p></div>
           </div>
           <div className="parallax__layer parallax__layer--base parallax__layer--left">
             {/* <div className="title box">Foreground Layer</div> */}

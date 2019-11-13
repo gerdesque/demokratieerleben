345d1b03f171a7b3bc325a08c4d21d8c51ee7f3b
Gerd Müller <gerdesk@posteo.org>
2019-11-13 22:48:48 +0100

    Add design ideas for intro chapter

--- src/routes/Intro.js
+++ src/routes/Intro.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {NavLink} from "react-router-dom";
 import './Intro.css';
 import Birds from '../Birds';
+import FadeInSection from '../FadeInSection';
 
 function Intro() {
   return (
@@ -19,18 +20,29 @@ function Intro() {
         </div>
         <div id="group2" className="parallax__group">
           <div className="parallax__layer parallax__layer--base">
-            <div className="box"><p>Es ist 1927. Irgendwo im Ruhrgebiet leben zwei Kinder: Anna und Karl. Die Eltern der Beiden gehören zur großen Arbeiterschaft, die im Bergbau, in Webereien, Fabriken und Stahlwerken arbeiten. Anna und Karl machen sich Gedanken über ihre Zukunft, denn sie sind 13 Jahre alt und ihr letztes Schuljahr steht bald bevor. Begleite sie dabei, wie sie ihren Weg zu einer „Kinderrepublik“ der sozialistischen Arbeiterjugend finden und was sie dort erleben.</p></div>
+            <div className="box"><p>Es ist 1927. Irgendwo im Ruhrgebiet leben zwei Kinder: Anna und Karl. Die Eltern der Beiden gehören zur großen Arbeiterschaft, die im Bergbau, in Webereien, Fabriken und Stahlwerken arbeiten.</p></div>
           </div>
           <div className="parallax__layer parallax__layer--deep"></div>
         </div>
         <div id="group3" className="parallax__group">
+          <div className="parallax__layer parallax__layer--base">
+            <div className="box"><p>Anna und Karl machen sich Gedanken über ihre Zukunft, denn sie sind 13 Jahre alt und ihr letztes Schuljahr steht bald bevor. Begleite sie dabei, wie sie ihren Weg zu einer „Kinderrepublik“ der sozialistischen Arbeiterjugend finden und was sie dort erleben.</p></div>
+          </div>
+          <div className="parallax__layer parallax__layer--deep"></div>
+        </div>
+        <div id="group4" className="parallax__group">
           <div className="parallax__layer parallax__layer--fore">
-          <div className="box"><p>Wen möchtest du auf diesem Weg begleiten?</p>
-            <NavLink exact to={{ pathname: "/chapterone", state: { avatar: "Karl" } }}>Karl</NavLink>
-            <NavLink exact to={{ pathname: "/chapterone", state: { avatar: "Anna" } }}>Anna</NavLink>
+            <div className="box decission">
+              <FadeInSection>
+                <p>Wen möchtest du auf diesem Weg begleiten?</p>
+                <NavLink exact to={{ pathname: "/chapterone", state: { avatar: "Karl" } }}>Karl</NavLink>
+                <NavLink exact to={{ pathname: "/chapterone", state: { avatar: "Anna" } }}>Anna</NavLink>
+            </FadeInSection>
+            </div>
           </div>
-          </div>
-          <div className="parallax__layer parallax__layer--base parallax__layer--left"></div>
+          <FadeInSection>
+            <div className="parallax__layer parallax__layer--base parallax__layer--left"></div>
+          </FadeInSection>
           <div className="parallax__layer parallax__layer--back parallax__layer--right"></div>
         </div>
       </div>
